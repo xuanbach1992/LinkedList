fe3d6fce7796653f54c58468e8d5c8471bf6c021
xuanbach1992 <Xuanbach1992>
2019-10-10 21:09:46 +0700

    fix linked list

--- LinkedList.php
+++ LinkedList.php
@@ -6,27 +6,54 @@ class LinkedList
 {
     public $firstNode;
     public $lastNode;
+    public $count;
 
     public function __construct()
     {
         $this->firstNode = null;
         $this->lastNode = null;
+        $this->count = 0;
     }
 
     public function addFisrtNode($data)
     {
         $node = new Node($data);
         if ($this->firstNode) {
-            $this->lastNode = $this->firstNode;
+//            $this->lastNode = $this->firstNode;
             $node->link = $this->firstNode;
         }
+        if (!$this->lastNode) {
+            $this->lastNode = $node;
+        }
         $this->firstNode = $node;
+        $this->count++;
     }
 
     public function addLastNode($data)
     {
-        $node=new Node($data){
-
+        $node = new Node($data);
+        if ($this->lastNode) {
+            $this->lastNode = $node;
+        }
+        $this->firstNode = $node;
+        $this->count++;
     }
+
+    public function addNode($index, $data)
+    {
+        if ($index == 0) {
+            $this->addFisrtNode($data);
+        } else {
+            $node = new Node($data);
+            $previous = $this->firstNode;
+            $current = $this->firstNode;
+            for ($i = 0; $i < $index; $i++) {
+                $previous = $current;
+                $current = $current->link;
+            }
+            $node->link = $current;
+            $previous->link = $node;
+        }
+        $this->count++;
     }
 }
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "'hello'";
+include_once "LinkedList.php";
+$myList=new LinkedList();
+$myList->addFisrtNode(2);
+$myList->addFisrtNode(3);
+$myList->addFisrtNode(4);
+$myList->addFisrtNode(5);
+
+print_r($myList);
